cc1a41cb1d8175d9d3fa7cfe373fba72e40fe2e6
hushino <frostshock@outlook.com>
2019-10-18 17:47:34 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const path = require('path');
+const multer = require('multer');
+//const uuidv4 = require('uuid/v4');
+var cors = require('cors');
+
+const storage = multer.diskStorage({
+    destination: path.join(__dirname, 'public/uploads'),
+    filename: (req, file, cb) => {
+        cb(null, /* uuidv4() +  "-originalname-" +*/ file.originalname/* .toLowerCase().trim() */ + ".png");
+    }
+})
+
+// Initializations
+const app = express();
+app.use(cors());
+// Settings
+app.set('port', 3003);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// Multer middleware
+app.use(multer({
+    storage,
+    dest: path.join(__dirname, 'public/uploads'),
+    limits: { fieldSize: 2000000 }, // 2mb
+    fileFilter: (req, file, cb) => {
+        const filetypes = /jpeg|jpg|png|gif/;
+        const mimetype = filetypes.test(file.mimetype);
+        const extname = filetypes.test(path.extname(file.originalname));
+        console.log(mimetype + " && " + extname)
+        if (mimetype /* && extname */) {
+            return cb(null, true)
+        }
+        cb("Error: archivo invalido")
+    }
+
+}).single('image'));
+
+// Routes
+app.use(require('./routes/routes'));
+
+// Static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Start the server
+app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`);
+})
+
+/* const imageInfoExampleOfMulterAfterUpload = {
+    fieldname: 'image',
+    originalname: 'ZMCZ-13181.jpg',
+    encoding: '7bit',
+    mimetype: 'image/jpeg',
+    destination:
+        'C:\\Users\\redmagic\\Desktop\\server\\server\\src\\public\\uploads',
+    filename: '6a8c756b768a15e14b5fd22efc50101e',
+    path:
+        'C:\\Users\\redmagic\\Desktop\\server\\server\\src\\public\\uploads\\6a8c756b768a15e14b5fd22efc50101e',
+    size: 65784
+} */
---
+++ src/routes/routes.js
@@ -0,0 +1,36 @@
+const { Router } = require('express');
+const router = Router();
+const fs = require('fs');
+const stream = require('stream');
+const path = require('path');
+
+// Routes
+router.get('/', (req, res) => {
+    res.render('index');
+});
+
+router.post('/upload', (req, res) => {
+    console.log(req.file);
+    res.send(req.file)
+});
+router.get('/upload/image/:id', (req, res) => {
+    //res.send(`Get Image Name with id ${req.params.id}`)
+    const pat = path.join(__dirname, `../public/uploads/${req.params.id}`)
+    const r = fs.createReadStream(pat) // or any other way to get a readable stream
+    const ps = new stream.PassThrough() // <---- this makes a trick with stream error handling
+    stream.pipeline(
+        r,
+        ps, // <---- this makes a trick with stream error handling
+        (err) => {
+            if (err) {
+                console.log(err) // No such file or any other kind of error
+                return res.sendStatus(400);
+            }
+        })
+    ps.pipe(res) // <---- this makes a trick with stream error handling
+});
+router.put('/upload/image/:id', (req, res) => {
+    res.send(req.file)
+});
+
+module.exports = router;
